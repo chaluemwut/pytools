82f9c7bbd5e5a7182dcddeac2c96fcdab1fefab3
chaluemwutnoyunsan <chaluemwut@hotmail.com>
2016-04-27 22:17:31 +0700

    first upload

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ import os
 from setuptools import setup
 
 setup(
-    name="pytools",
+    name="py3tools",
     version="0.0.1",
     description="Python3 tools",
     author='Chaluemwut Noyunsan',
     packages=["util", "db"],
-
+    url='https://github.com/chaluemwut/pytools.git',
+    download_url='https://github.com/chaluemwut/pytools.git'
 )
