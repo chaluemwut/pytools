f3b8c544c0e99f66a5d4911f55029288594ee1aa
chaluemwutnoyunsan <chaluemwut@hotmail.com>
2016-04-27 21:55:37 +0700

    update project

---
+++ setup.py
@@ -0,0 +1,11 @@
+import os
+from setuptools import setup
+
+setup(
+    name="pytools",
+    version="0.0.1",
+    description="Python3 tools",
+    author='Chaluemwut Noyunsan',
+    packages=["util", "db"],
+
+)
